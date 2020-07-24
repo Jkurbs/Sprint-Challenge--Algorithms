b40eacfc23e418e7be954ec33a96a29a289bfb51
Kerby Jean <Kurbs@Kerbys-MacBook-Pro.local>
2020-07-24 13:11:46 -0400

    Done count word

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -5,6 +5,15 @@ Your function must utilize recursion. It cannot contain any loops.
 '''
 def count_th(word):
     
-    # TBC
-    
-    pass
+    # Verify the lenght of the word 
+    if len(word) < 2:
+        return 0
+
+    # Make word lower case and verify if following 2 words equal to "th"
+    elif word[0:2].lower() == "th":
+        # If it is return count + 1
+        return count_th(word[2:]) + 1 
+    else:
+        return count_th(word[1:])
+
+print(count_th("brniobwnrwivqnieorTHth"))
